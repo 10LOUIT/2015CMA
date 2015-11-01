439c03dbafd065d7b4adfa82befa6c9029f93d23
10LOUIT <thorsten.louis@gmx.de>
2015-11-01 21:38:56 +0100

    refactoring and new file added

--- RFM/myRFM.R
+++ RFM/myRFM.R
@@ -7,47 +7,42 @@
 
 rm(list=ls(all=TRUE))
 setwd("~/Documents/github/2015CMA/RFM")
-
+setwd("~/OneDrive/B_Data/Berner_PT")
 # --- COMPUTING RECENCY, FREQUENCY, MONETARY VALUE ---------
 
 # Load necessary packages
-library(RSQLite)
 library(DBI)
-library(gsubfn)
+library(RSQLite)
 library(proto)
+library(gsubfn)
 library(tcltk)
 
 # Compute key marketing indicators using SQL language
 library(sqldf)
 
 # Load text file into local variable called 'data'
-setwd("~/OneDrive/B_Data/Berner_PT")
 raw_data = read.delim(file = 'Customer_purchases_2011_2013_with_empty_amounts.csv', header = FALSE, sep = ',', dec = '.')
-#summary(raw_data)
 
 # Manipulate data of old customers without purchases in this file
+
+# Select all customers with transactions in the selected period 
 new_data = sqldf("SELECT * FROM raw_data WHERE V3 > 0")
+
+# Select all customers without any transactions in the selected period
 old_data = sqldf("SELECT * FROM raw_data WHERE V3 = 0")
-negative_data = sqldf("SELECT * FROM raw_data WHERE V2 < 0")
-old_data$V3 = "2010-01-01" 
+
+# Assignment of purchase amount and date of purchase
+old_data$V3 = "2008-01-01" 
 old_data$V2 = "0.00"
+
+# Select all customers with negative purchase in the selected period
+# negative_data = sqldf("SELECT * FROM raw_data WHERE V2 < 0")
+
+# Merge the processed data
 data = merge(new_data, old_data, all.x = TRUE, all.y = TRUE)
+# Filtering out all transactions with a negative amount of purchase
 data = sqldf("SELECT * FROM raw_data WHERE V2 > -0.01")
 #data = sqldf("SELECT * FROM raw_data WHERE V2 > -0.01 AND V3 NOT LIKE 0")
-
-# Explore customer data with negative amounts
-negative_data$V3 = as.Date(negative_data$V3, "%Y-%m-%d")
-hist(negative_data$V2)
-summary(negative_data$V2)
-plot(negative_data$V2, negative_data$V3)
-
-# Cluster Analysis
-cluster_data = negative_data
-cluster_data$V3 = as.numeric(difftime(time1 = "2014-01-01",time2 = cluster_data$V3, units = "days"))
-clusters = kmeans(cluster_data, 5, 10)
-table(clusters$size)
-hist(clusters$size)
-plot(clusters$cluster, clusters$fitted.values)
 
 # Filter customers purchases smaller than a distinct threshold 
 #data = sqldf("SELECT * FROM customers_with_purchases WHERE V2 > 10")
@@ -110,21 +105,9 @@ rfmSegmentation <- function(dataInput, orderValueThreshold = 100, endDate = 0) {
   dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$amount < orderValueThreshold)] = "active low value"
   dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$amount >= orderValueThreshold)] = "active high value"
  
-  
+  # return data
   dataOutput
   
-}
-
-#thorsten: my pie chart function
-myPieChart <- function(dataInput, chartName){
-  # Plot a pie chart
-  mytable <- table(dataInput$segment)
-  slices <- c(mytable)
-  pct <- round(slices/sum(slices)*100, digits = 2)
-  lbls <- paste(names(mytable), "\n", mytable, sep="")
-  lbls <- paste(lbls, ",", pct) # add percents to labels 
-  lbls <- paste(lbls,"%",sep="") # ad % to labels 
-  pie(mytable, labels = lbls, col = rainbow(length(lbls)), main = chartName)
 }
 
 # Call the rfm segmentation function and return the processed df
@@ -144,7 +127,6 @@ table(customers_2011$segment)
 
 # --- PREPARING AND TRANSFORMING DATA ----------------------
 
-
 # Copy customer data into new data frame
 new2_data = sqldf("SELECT * FROM customers WHERE amount > 0")
 summary(new2_data)
@@ -163,26 +145,9 @@ hist(new2_data$amount)
 new_data2 = scale(new_data)
 head(new2_data)
 
-# Call the pie chart functions
-myPieChart(customers, "Pie Chart of Customer Segments\n (Portugal)")
-
+# Data Exploration and visualization
+myPieChart(customers, "Pie Chart of Customer Segments\n (RFM)")
 aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# Display the data after transformation
-#head(data)
-#summary(data)
-
-
-# Compute key marketing indicators using SQL language
-# library(sqldf)
-
-# Compute recency, frequency, and average purchase amount
-# customers_2013 = sqldf("SELECT customer_id,
-#                                MIN(days_since) AS 'recency',
-#                                MAX(days_since) AS 'first_purchase',
-#                                COUNT(*) AS 'frequency',
-#                                AVG(purchase_amount) AS 'amount'
-#                         FROM data GROUP BY 1")
 
 # Explore the data
 head(customers_2013)
@@ -192,105 +157,15 @@ hist(customers_2013$frequency)
 hist(customers_2013$amount)
 hist(customers_2013$amount, breaks = 100)
 
-# --- CODING A MANAGERIAL SEGMENTATION ---------------------
-
-# Simple 2-segment solution based on recency alone
-# customers_2013$segment = ifelse(test = customers_2013$recency > 365*3, yes = "inactive", no = "NA")
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# A more complex 3-segment solution based on recency alone
-# customers_2013$segment = ifelse(test = customers_2013$recency > 365*3,
-#                            yes = "inactive",
-#                            no = ifelse(test = customers_2013$recency > 365*2,
-#                                        yes = "cold",
-#                                        no = "NA"))
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# Simple 2-segment solution using the which statement
-# customers_2013$segment = "NA"
-# customers_2013$segment[which(customers_2013$recency > 365*3)] = "inactive"
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# More complex 4-segment solution using which
-# customers_2013$segment = "NA"
-# customers_2013$segment[which(customers_2013$recency > 365*3)] = "inactive"
-# customers_2013$segment[which(customers_2013$recency <= 365*3 & customers_2013$recency > 365*2)] = "cold"
-# customers_2013$segment[which(customers_2013$recency <= 365*2 & customers_2013$recency > 365*1)] = "warm"
-# customers_2013$segment[which(customers_2013$recency <= 365)] = "active"
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# Complete segment solution using which, and exploiting previous test as input
-# customers_2013$segment = "NA"
-# customers_2013$segment[which(customers_2013$recency > 365*3)] = "inactive"
-# customers_2013$segment[which(customers_2013$recency <= 365*3 & customers_2013$recency > 365*2)] = "cold"
-# customers_2013$segment[which(customers_2013$recency <= 365*2 & customers_2013$recency > 365*1)] = "warm"
-# customers_2013$segment[which(customers_2013$recency <= 365)] = "active"
-# customers_2013$segment[which(customers_2013$segment == "warm" & customers_2013$first_purchase <= 365*2)] = "new warm"
-# customers_2013$segment[which(customers_2013$segment == "warm" & customers_2013$amount < 100)] = "warm low value"
-# customers_2013$segment[which(customers_2013$segment == "warm" & customers_2013$amount >= 100)] = "warm high value"
-# customers_2013$segment[which(customers_2013$segment == "active" & customers_2013$first_purchase <= 365)] = "new active"
-# customers_2013$segment[which(customers_2013$segment == "active" & customers_2013$amount < 100)] = "active low value"
-# customers_2013$segment[which(customers_2013$segment == "active" & customers_2013$amount >= 100)] = "active high value"
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-# Re-order factor in a way that makes sense
-# customers_2013$segment = factor(x = customers_2013$segment, levels = c("inactive", "cold",
-#                                                              "warm high value", "warm low value", "new warm",
-#                                                              "active high value", "active low value", "new active"))
-# table(customers_2013$segment)
-# aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
-
-
 # --- SEGMENTING A DATABASE RETROSPECTIVELY ----------------
-
+# Segment customers with transactions in the previous year
 customers_2012 <- rfmSegmentation(data, 150, 365)
-# Compute key marketing indicators using SQL language
-# library(sqldf)
-# 
-# # Compute recency, frequency, and average purchase amount
-# customers_2012 = sqldf("SELECT customer_id,
-#                                MIN(days_since) - 365 AS 'recency',
-#                                MAX(days_since) - 365 AS 'first_purchase',
-#                                COUNT(*) AS 'frequency',
-#                                AVG(purchase_amount) AS 'amount'
-#                         FROM data
-#                         WHERE days_since > 365
-#                         GROUP BY 1")
-# 
-# # Complete segment solution using which, and exploiting previous test as input
-# customers_2012$segment = "NA"
-# customers_2012$segment[which(customers_2012$recency > 365*3)] = "inactive"
-# customers_2012$segment[which(customers_2012$recency <= 365*3 & customers_2012$recency > 365*2)] = "cold"
-# customers_2012$segment[which(customers_2012$recency <= 365*2 & customers_2012$recency > 365*1)] = "warm"
-# customers_2012$segment[which(customers_2012$recency <= 365)] = "active"
-# customers_2012$segment[which(customers_2012$segment == "warm" & customers_2012$first_purchase <= 365*2)] = "new warm"
-# customers_2012$segment[which(customers_2012$segment == "warm" & customers_2012$amount < 100)] = "warm low value"
-# customers_2012$segment[which(customers_2012$segment == "warm" & customers_2012$amount >= 100)] = "warm high value"
-# customers_2012$segment[which(customers_2012$segment == "active" & customers_2012$first_purchase <= 365)] = "new active"
-# customers_2012$segment[which(customers_2012$segment == "active" & customers_2012$amount < 100)] = "active low value"
-# customers_2012$segment[which(customers_2012$segment == "active" & customers_2012$amount >= 100)] = "active high value"
-# 
-# # Re-order factor in a way that makes sense
-# customers_2012$segment = factor(x = customers_2012$segment, levels = c("inactive", "cold",
-#                                                                        "warm high value", "warm low value", "new warm",
-#                                                                        "active high value", "active low value", "new active"))
-# 
-# # Show segmentation results
-# table(customers_2012$segment)
-# pie(table(customers_2012$segment), col = rainbow(24))
 
 # Show segmentation results
-myPieChart(customers_2012, "Pie Chart of Customer Segments\n (Portugal 2012)")
+myPieChart(customers_2012, "Pie Chart of Customer Segments\n (RFM 2012)")
 aggregate(x = customers_2012[, 2:5], by = list(customers_2012$segment), mean)
 
-
 # --- COMPUTING REVENUE GENERATION PER SEGMENT -------------
-
 
 # Compute how much revenue is generated by segments
 # Notice that people with no revenue in 2013 do NOT appear
@@ -300,15 +175,22 @@ revenue_2013 = sqldf("SELECT customer_id, SUM(purchase_amount) AS 'revenue_2013'
                       GROUP BY 1")
 summary(revenue_2013)
 
-# Merge 2013 customers and 2013 revenue (the wrong way)
-# actual = merge(customers_2013, revenue_2013)
-
 # Merge 2013 customers and 2013 revenue (correct)
 actual = merge(customers_2013, revenue_2013, all.x = TRUE)
 actual$revenue_2013[is.na(actual$revenue_2013)] = 0
+myPieChart(actual, "Pie Chart of Customer Segments\n (actual RFM 2013)")
 
 # Show average revenue per customer and per segment
-aggregate(x = actual$revenue_2013, by = list(customers_2013$segment), mean)
+actual_aggregation = aggregate(x = actual$revenue_2013, by = list(customers_2013$segment), mean)
+# Re-order and display results
+actual_aggregation = actual_aggregation[order(actual_aggregation$x, decreasing = TRUE), ]
+print(actual_aggregation)
+barplot(actual_aggregation$x, names.arg = actual_aggregation$Group.1, legend.text = round(actual_aggregation$x, 2), xlab = "Customer Segments", ylab = "Average revenue", col = c("darkgreen","green","orange","yellow","darkblue","blue","red"), main="Average revenue per customer and per segment")
+
+# Show total revenue per customer and per segment
+actual_total = aggregate(x = actual$revenue_2013, by = list(customers_2013$segment), sum)
+actual_total = actual_total[order(actual_total$x, decreasing = TRUE), ]
+barplot(actual_total$x, names.arg = actual_total$Group.1, legend.text = round(actual_total$x, 2), xlab = "Customer Segments", ylab = "total revenue", col = c("darkgreen","green","orange","yellow","darkblue","blue","red"), main="Total revenue per customer and per segment")
 
 # Merge 2012 customers and 2013 revenue (correct)
 forward = merge(customers_2012, revenue_2013, all.x = TRUE)
@@ -321,4 +203,4 @@ print(r)
 # Re-order and display results
 r = r[order(r$x, decreasing = TRUE), ]
 print(r)
-barplot(r$x, names.arg = r$Group.1)
+barplot(r$x, names.arg = r$Group.1, legend.text = round(r$x, 2), xlab = "Customer Segments", ylab = "Average revenue", col = c("darkgreen","green","orange","yellow","darkblue","blue","red"), main="Average revenue per customer and per segment")
