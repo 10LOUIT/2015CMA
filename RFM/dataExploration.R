bfb744ee41da238caaed6e72a07bec86c161be71
10LOUIT <thorsten.louis@gmx.de>
2015-11-08 11:23:16 +0100

    a lot

--- RFM/dataExploration.R
+++ RFM/dataExploration.R
@@ -5,16 +5,18 @@
 # __________________________________________________________
 # //////////////////////////////////////////////////////////
 
+library(ggplot2)
+
 # Explore customer data with negative amounts
-negative_data$V3 = as.Date(negative_data$V3, "%Y-%m-%d")
-hist(negative_data$V2)
-summary(negative_data$V2)
-plot(negative_data$V2, negative_data$V3)
+negative_data$date_of_purchase = as.Date(negative_data$date_of_purchase, "%Y-%m-%d")
+hist(negative_data$purchase_amount)
+summary(negative_data$purchase_amount)
+plot(negative_data$purchase_amount, negative_data$date_of_purchase)
 
 # ------------------------------------------------------------------------------------------------------------
 # Cluster Analysis
 cluster_data = negative_data
-cluster_data$V3 = as.numeric(difftime(time1 = "2014-01-01",time2 = cluster_data$V3, units = "days"))
+cluster_data$date_of_purchase = as.numeric(difftime(time1 = "2014-01-01",time2 = cluster_data$date_of_purchase, units = "days"))
 clusters = kmeans(cluster_data, 5, 10)
 table(clusters$size)
 hist(clusters$size)
@@ -43,12 +45,20 @@ mydata = mydata[,-3]
 mydata <- scale(mydata) # standardize variables
 
 # Filtering out Customers with order and following return
-customers_with_negative_transactions = sqldf("SELECT * FROM customers_with_transactions WHERE V2 < 0")
-customers_with_positive_transactions = sqldf("SELECT * FROM customers_with_transactions WHERE V2 > 0")
-customers_with_order_and_return = sqldf("SELECT customers_with_positive_transactions.V1, customers_with_positive_transactions.V2, customers_with_positive_transactions.V3, customers_with_negative_transactions.V2, customers_with_negative_transactions.V3 FROM customers_with_positive_transactions INNER JOIN customers_with_negative_transactions ON customers_with_positive_transactions.V1 = customers_with_negative_transactions.V1 WHERE NOT -customers_with_negative_transactions.V2 = customers_with_positive_transactions.V2")
+customers_with_negative_transactions = sqldf("SELECT * FROM raw_data_6 WHERE purchase_amount < 0")
+colSums(customers_with_negative_transactions[2])
+
+customers_with_positive_transactions = sqldf("SELECT * FROM raw_data_6 WHERE purchase_amount > 0")
+colSums(customers_with_positive_transactions[2])
+
+customers_clean = sqldf("SELECT * FROM customers_with_positive_transactions INNER JOIN customers_with_negative_transactions ON customers_with_positive_transactions.customer_id = customers_with_negative_transactions.customer_id WHERE NOT -customers_with_negative_transactions.purchase_amount = customers_with_positive_transactions.purchase_amount")
+customers_clean = sqldf("SELECT * FROM customers_with_positive_transactions as T1 JOIN customers_with_negative_transactions as T2 ON NOT T1.customer_id = T2.customer_id AND NOT T1.purchase_amount = -T2.purchase_amount")
+
+# DELETE FROM `table1` WHERE `group` in (SELECT DISTINCT `group` FROM `table2`)
+customers_clean = sqldf("DELETE FROM customers_with_positive_transactions WHERE customer_id IN (SELECT customer_id FROM customers_with_negative_transactions WHERE customers_with_positive_transactions.purchase_amount = -customers_with_negative_transactions.purchase_amount)")
 
 # Delete those transactions (not finish)
-customers_without_returns = sqldf("SELECT * FROM customers_with_positive_transactions INNER JOIN customers_with_negative_transactions ON customers_with_positive_transactions.V1 = customers_with_negative_transactions.V1 WHERE NOT -customers_with_negative_transactions.V2 = customers_with_positive_transactions.V2")
+customers_without_returns = sqldf("SELECT * FROM customers_with_positive_transactions INNER JOIN customers_with_negative_transactions ON customers_with_positive_transactions.customer_id = customers_with_negative_transactions.customer_id WHERE NOT -customers_with_negative_transactions.purchase_amount = customers_with_positive_transactions.purchase_amount")
 rm(customers_without_returns)
 
 # ------------------------------------------------------------------------------------------------------------
@@ -76,3 +86,52 @@ print(d)
 barplot(as.matrix(dm), main="Comparing Customer Segments", ylab = "Numbers", cex.lab = 1.5, cex.main = 1.4, beside=TRUE, col=rainbow(8))
 
 # ------------------------------------------------------------------------------------------------------------
+
+# --------- Data Exploration and visualization -----------
+
+myPieChart(customers_2012, "Pie Chart of Customer Segments\n (RFM 2012)")
+aggregate(x = customers_2012[, 2:5], by = list(customers_2012$segment), mean)
+aggretation_2012 = aggregate(x = customers_2012$amount, by = list(customers_2012$segment), mean)
+aggretation_2012 = aggretation_2012[order(aggretation_2012$Group.1, decreasing = TRUE), ]
+
+myPieChart(customers_2013, "Pie Chart of Customer Segments\n (RFM 2013)")
+aggregate(x = customers_2013[, 2:5], by = list(customers_2013$segment), mean)
+aggretation_2013 = aggregate(x = customers_2013$amount, by = list(customers_2013$segment), mean)
+aggretation_2013 = aggretation_2013[order(aggretation_2013$Group.1, decreasing = TRUE), ]
+
+myPieChart(customers_2014, "Pie Chart of Customer Segments\n (RFM 2014)")
+aggregate(x = customers_2014[, 2:5], by = list(customers_2014$segment), mean)
+aggretation_2014 = aggregate(x = customers_2014$amount, by = list(customers_2014$segment), mean)
+aggretation_2014 = aggretation_2014[order(aggretation_2014$Group.1, decreasing = TRUE), ]
+
+myPieChart(customers_2015, "Pie Chart of Customer Segments\n (RFM 2015)")
+aggregate(x = customers_2015[, 2:5], by = list(customers_2015$segment), mean)
+aggretation_2015 = aggregate(x = customers_2015$amount, by = list(customers_2015$segment), mean)
+aggretation_2015 = aggretation_2015[order(aggretation_2015$Group.1, decreasing = TRUE), ]
+
+#aggretation_customers_2015 = aggregate(x = customers_2015$customer_id, by = list(customers_2015$segment), mean)
+
+# Show results in console
+print(aggretation_2012)
+print(aggretation_2013)
+print(aggretation_2014)
+print(aggretation_2015)
+
+# Create data object average purchase amount per segments over years
+avg_revenue_per_segment_1 = merge(aggretation_2014, aggretation_2015, by = "Group.1", all.x = TRUE, all.y = TRUE)
+colnames(avg_revenue_per_segment_1) = c("Segment", "2014", "2015")
+avg_revenue_per_segment_2 = merge(aggretation_2012, aggretation_2013, by = "Group.1", all.x = TRUE, all.y = TRUE)
+colnames(avg_revenue_per_segment_2) = c("Segment", "2012", "2013")
+avg_revenue_per_segment = merge(avg_revenue_per_segment_2, avg_revenue_per_segment_1, by = "Segment", all.x = TRUE, all.y = TRUE)
+avg_revenue_per_segment$`2012`[is.na(avg_revenue_per_segment$`2012`)] = 0
+
+# Calculate ratio of change per customer segment compared to the previous year (inf)
+avg_revenue_per_segment$Ratio = as.numeric(round((avg_revenue_per_segment$`2015`/avg_revenue_per_segment$`2014`)*100), digits = 2)
+
+# Save average revenue per customer and per segment
+yearly_revenue = aggretation_2014$x
+yearly_revenue
+
+# Compare revenue per customer
+compare_revenue = merge(revenue_2014, revenue_2015)
+plot(compare_revenue$revenue_2014, x = compare_revenue$customer_id)
--- RFM/myFunctions.R
+++ RFM/myFunctions.R
@@ -35,7 +35,7 @@ rfmSegmentation <- function(dataInput, orderValueThreshold = 100, endDate = 0) {
                        COUNT(*) AS 'frequency',
                        AVG(purchase_amount) AS 'amount'
                        FROM dataInput 
-                       WHERE days_since > 365
+                       WHERE days_since > 360
                        GROUP BY 1")
     
   } else if(endDate==730){
@@ -49,21 +49,51 @@ rfmSegmentation <- function(dataInput, orderValueThreshold = 100, endDate = 0) {
                        FROM dataInput 
                        WHERE days_since > 730
                        GROUP BY 1")
+    
+  } else if(endDate==1095){
+    print(endDate)
+    # must be solved v <- endDate
+    dataOutput = sqldf("SELECT customer_id,
+                       MIN(days_since) - 1095 AS 'recency',
+                       MAX(days_since) - 1095 AS 'first_purchase',
+                       COUNT(*) AS 'frequency',
+                       AVG(purchase_amount) AS 'amount'
+                       FROM dataInput 
+                       WHERE days_since > 1095
+                       GROUP BY 1")
   }
   
   dataOutput$segment = "long time inactive"
-  dataOutput$segment[which(dataOutput$recency > 365*3)] = "inactive"
-  dataOutput$segment[which(dataOutput$recency <= 365*3 & dataOutput$recency > 365*2)] = "cold"
-  dataOutput$segment[which(dataOutput$recency <= 365*2 & dataOutput$recency > 365*1)] = "warm"
-  dataOutput$segment[which(dataOutput$recency <= 365)] = "active"
-  dataOutput$segment[which(dataOutput$segment == "warm" & dataOutput$first_purchase <= 365*2)] = "new warm"
+  dataOutput$segment[which(dataOutput$recency > 730)] = "inactive"
+  dataOutput$segment[which(dataOutput$recency <= 730 & dataOutput$recency > 365)] = "cold"
+  dataOutput$segment[which(dataOutput$recency <= 365 & dataOutput$recency > 182.5)] = "warm"
+  dataOutput$segment[which(dataOutput$recency <= 182.5)] = "active"
+  dataOutput$segment[which(dataOutput$segment == "warm" & dataOutput$first_purchase <= 365)] = "new warm"
   dataOutput$segment[which(dataOutput$segment == "warm" & dataOutput$amount < orderValueThreshold)] = "warm low value"
   dataOutput$segment[which(dataOutput$segment == "warm" & dataOutput$amount >= orderValueThreshold)] = "warm high value"
-  dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$first_purchase <= 365)] = "new active"
+  dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$first_purchase <= 182.5)] = "new active"
   dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$amount < orderValueThreshold)] = "active low value"
   dataOutput$segment[which(dataOutput$segment == "active" & dataOutput$amount >= orderValueThreshold)] = "active high value"
+  dataOutput$segment = factor(x = dataOutput$segment, levels = c("inactive", "cold", "warm high value", "warm low value", "new warm", "active high value", "active low value", "new active"))
   
   # return data
   dataOutput
   
-  }
+}
+# --- COMPUTE TRANSITION MATRIX ------------------------------------------------------------------------------------
+
+transitionMatrix <- function(dataPrevYear, dataActualYear){
+  
+  # Compute transition matrix
+  new_data = merge(x = dataPrevYear, y = dataActualYear, by = "customer_id", all.x = TRUE)
+  head(new_data)
+  transition = table(new_data$segment.x, new_data$segment.y)
+  print(transition)
+  
+  # Divide each row by its sum
+  transition = transition / rowSums(transition)
+  print(transition) 
+  
+  # Return transition probabilities per customer segement
+  transition
+}
